0542df4060a6635c660758b9a953b7f23b29505a
Ethan Cannelongo <ethancannelongo@gmail.com>
2025-04-24 15:49:49 -0400

    Challenge fixes

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
-# Step 1: Ask the user for the first number
-# Step 2: Ask the user for the operator (+, -, *, or /)
-# Step 3: Ask the user for the second number
+# Step 1: Ask the user for the first number and store it in a variable
+# Step 2: Ask the user for the operator (+, -, *, or /) and store it in a variable
+# Step 3: Ask the user for the second number and store it in a variable
 # Step 4: Check which operation the user chose and perform it using if and elif statements
     # 4.1 If the operator is +, add the numbers and print the result
     # 4.2 If the operator is -, subtract the numbers and print the result

27409248353ab6b50065ccc1ce91d02f9d94fb19
Hudson Cannelongo <hudsoncannelongo@gmail.com>
2025-04-24 12:07:18 -0400

    password changes

---
+++ login.py
@@ -0,0 +1,22 @@
+# 1. Make variables for username and password -
+# 2. Print to the user and ask for a password -
+# 3. get the typed in password from the user -
+# 4. Check if the password is correct -
+# 5. If its correct, then print access granted
+# 6. But if it is not correct, print access denine
+
+username = "hudson" 
+password = "jesus" 
+
+
+
+print ( " hello " + username + " what is your password'? " )
+
+passwordatempt = input (" please enter your password " )
+
+
+if passwordatempt >= password:
+    print (" acess granted :) ")
+
+else: 
+    print(" acess denide :( ")
---
+++ store.py
@@ -0,0 +1,21 @@
+# 1. Make a variable called product for your product -
+# 2. Make a variable called cost for the cost of your product -
+# 3. print hello would you like to buy the product to the user -
+# 4. Ask the user to how much money they have and put that into a variable called usermoney -
+# 5. check if the user has enough money to buy the product >=
+# 6 If he does, then print You have enough money!
+# 7. If he doesn't then get lost!
+
+product = " applewatch " 
+
+cost = 40 
+
+print (" Hello would you like to buy our apple watches? ")
+
+usermoney = input (" how much money is in you wallet? ")
+
+if int (usermoney) >= cost:
+    print ( " you may buy our product ")
+
+else: 
+    print (" get lost you homeless person!!! ") 

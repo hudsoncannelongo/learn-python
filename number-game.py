3fbf197f291c3b5aede92cbf8d4b7ecf1a6b697a
Ethan Cannelongo <33533047+ethancanne@users.noreply.github.com>
2025-04-29 20:27:27 -0400

    Added a new challenge, and its a game!

---
+++ number-game.py
@@ -0,0 +1,16 @@
+# 1. First, we need to get random numbers, so: import random
+
+# 2. Make a variable that picks a random number between 1 and 10: random.randint(1, 10)
+
+# 3. Ask the user to guess a number using input()
+# Turn it into a number with int()
+
+# 4. Use a while loop to loop every time the guess is NOT equal to the secret number
+
+    # 5. If the guess is too high, print "Too high!"
+
+    # 6. If the guess is too low, print "Too low!"
+
+    # 7. Ask again and get a new guess
+
+# 8. When the guess is right, print "You got it!" in green
